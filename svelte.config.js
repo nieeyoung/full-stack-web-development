46d02ab2722c24c37473941ac5be0536decfa708
nieeyoung <pdekoya@gmail.com>
2022-09-14 22:20:38 +0000

    Configure HMR on Gitpod & localhost.

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,16 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		target: '#svelte',
+		vite: {
+			server: {
+				hmr:{
+					clientPort: process.env.HMR_HOST ? 443 : 5173,
+					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://.length") : "localhost"
+				}
+			}
+		}
 	}
 };
 
